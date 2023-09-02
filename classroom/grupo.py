4d599b0419ea6432e6e5e316e61e18019ec0c4b0
daninjaa <dagarciat@unal.edu.co>
2023-09-02 16:21:17 -0500

    commit2

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,16 +6,20 @@ class Grupo:
     def __init__(self, grupo="grupo predeterminado", asignaturas = [], estudiantes = [], grado = "Grado 12"):
         self._grupo = grupo
         self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes.sort()
+        self.listadoAlumnos = estudiantes
+        self.listadoAlumnos.sort()
         Grupo.grado = grado
 
     def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos.sort()
         
 
     def __str__(self):
